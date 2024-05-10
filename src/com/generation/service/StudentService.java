c918a33605c39a05617323cc197252fc9b837f86
Tanna <tanna1987@gmail.com>
2024-05-10 15:10:45 +0800

    first commit

---
+++ src/com/generation/service/StudentService.java
@@ -0,0 +1,41 @@
+package com.generation.service;
+
+import com.generation.model.Course;
+import com.generation.model.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentService {
+    private final Map<String, Student> students = new HashMap<>();
+
+    public void subscribeStudent(Student student) {
+        students.put(student.getId(), student);
+    }
+
+    public Student findStudent(String studentId) {
+        if (students.containsKey(studentId)) {
+            return students.get(studentId);
+        }
+        return null;
+    }
+
+    public boolean isSubscribed(String studentId) {
+        //TODO DONE
+        return students.containsKey(studentId);
+    }
+
+    public void showSummary() {
+        //TODO DONE
+        System.out.println(students.toString());
+
+    }
+
+    public void enrollToCourse(String studentId, Course course) {
+        if (students.containsKey(studentId)) {
+            students.get(studentId).enrollToCourse(course);
+        }
+    }
+
+
+}
